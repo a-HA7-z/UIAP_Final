2468866925d162769ab05c129ffe839970eb5d19
a-HA7-z <hosseinazizpour76@gmail.com>
2025-07-07 17:34:40 +0330

    Initial DepositAccount

---
+++ DepositAccount.cpp
@@ -0,0 +1,11 @@
+#include "DepositAccount.h"
+
+DepositAccount::DepositAccount(const string& cardNumber, const string& accountNumber, const string& IBAN_number,
+                               const string& CVV2, const string& Exp_Date, const string& PIN,
+                               const string& staticPassword, long long int balance) :
+        BankAccount(cardNumber,accountNumber,IBAN_number,CVV2,Exp_Date,PIN,staticPassword,balance) {}
+
+double DepositAccount::calculateInterest(int months) {
+    double rate = 0.1;
+    return (getBalance() * rate * months) / 12.0;
+}
