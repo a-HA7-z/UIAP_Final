2389f844be5512a2edc3e326d5cf335652255841
a-HA7-z <hosseinazizpour76@gmail.com>
2025-07-07 17:24:54 +0330

    Initial CurrentAccount

---
+++ CurrentAccount.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "CurrentAccount.h"
+
+CurrentAccount::CurrentAccount(const string& cardNumber, const string& accountNumber, const string& IBAN_number,
+                               const string& CVV2, const string& Exp_Date, const string& PIN,
+                               const string& staticPassword, long long int balance) :
+                               BankAccount(cardNumber,accountNumber,IBAN_number,CVV2,Exp_Date,PIN,staticPassword,balance) {}
+
+void CurrentAccount::issueCheque() {
+    cout << "Issuing a cheque from current account..." << endl;
+}
---
+++ CurrentAccount.h
@@ -0,0 +1,12 @@
+#include "BankAccount.h"
+using namespace std;
+
+class CurrentAccount : public BankAccount
+{
+public:
+    CurrentAccount(const string& cardNumber, const string& accountNumber, const string& IBAN_number,
+                   const string& CVV2, const string& Exp_Date, const string& PIN,
+                   const string& staticPassword, long long int balance);
+
+    void issueCheque();
+};
